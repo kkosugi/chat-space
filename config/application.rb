1b2008bc4cbf6ed5acfd7017a4052f18192fa358
kohei kosugi <klam195310@gmail.com>
2017-09-15 19:24:15 +0900

    create message function

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,11 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+      config.i18n.default_locale = :ja
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
+  resources :messages, only: [:index, :create]
   resources :groups, only: [:index, :create, :new, :edit, :update] do
-    resources :messages, only:[:index]
+    resources :messages, only:[:index,]
   end
   resources :users, only: [:edit, :update]
 
