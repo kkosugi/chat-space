2a0d161908ac56b2ae76e04f5dcacef60905d637
kohei kosugi <klam195310@gmail.com>
2017-09-15 19:55:42 +0900

    Edit message migrate

--- db/migrate/20170915105052_create_messages.rb
+++ db/migrate/20170915105052_create_messages.rb
@@ -1,10 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.text    :text
+      t.text    :text, null: false
       t.text    :image
-      t.integer :user_id
-      t.integer :group_id
+      t.integer :user_id, null: false, foreign_key: true
+      t.integer :group_id, null: false, foreign_key: true
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170915072050) do
+ActiveRecord::Schema.define(version: 20170915105052) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -28,10 +28,10 @@ ActiveRecord::Schema.define(version: 20170915072050) do
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "text",       limit: 65535
+    t.text     "text",       limit: 65535, null: false
     t.text     "image",      limit: 65535
-    t.integer  "user_id"
-    t.integer  "group_id"
+    t.integer  "user_id",                  null: false
+    t.integer  "group_id",                 null: false
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
