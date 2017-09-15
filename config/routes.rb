8b32565bccd0c13c962d0fce5c82f90d3524e00b
kohei kosugi <klam195310@gmail.com>
2017-09-15 19:37:50 +0900

    Remove messages route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :messages, only: [:index, :create]
   resources :groups, only: [:index, :create, :new, :edit, :update] do
-    resources :messages, only:[:index,]
+    resources :messages, only:[:index, :create]
   end
   resources :users, only: [:edit, :update]
 
