33e03f788f03f9e113e37910fd619f3b2a65d69a
kohei kosugi <klam195310@gmail.com>
2017-09-17 16:35:04 +0900

    Add validate

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,10 @@ class Message < ApplicationRecord
   belongs_to :group
 
   mount_uploader :imge, ImageUploader
-  validates :text, presence: true
+  validates :text, presence: true, unless: :image
+
+  private
+    def image
+      image = presence
+    end
 end
