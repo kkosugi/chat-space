27adfb387e786a1013033fa8e9a9683694c5252f
kohei kosugi <klam195310@gmail.com>
2017-09-30 15:21:15 +0900

    Edit indent

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,9 @@
 $(function(){
   function buildHTML(message){
     var insertImage = " ";
-     if (message.image.url) {
-       insertImage = `<img src="${message.image.url}" width="300" height=nil>`;
-     }
+     　if (message.image.url) {
+       　insertImage = `<img src="${message.image.url}" width="300" height=nil>`;
+     　}
     var html =
     `<div class = chat-main__body__messageslist data-message-id="${message.id}">
       <div class = chat-main__body__messageslist__name>
@@ -21,49 +21,48 @@ $(function(){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.chat-main__body').append(html)
-      $('.input').val('')
-      $('.btn').val('')
-      $('.html,body').animate({scrollTop: $('.chat-main__body')[0].scrollHeight}, 'fast');
-    })
-    .fail(function() {
-      alert('投稿できませんでした')
-    })
-    return false;
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.chat-main__body').append(html)
+        $('.input').val('')
+        $('.btn').val('')
+        $('.html,body').animate({scrollTop: $('.chat-main__body')[0].scrollHeight}, 'fast');
+      })
+      .fail(function() {
+        alert('投稿できませんでした')
+      })
+  return false;
   });
 
     var interval = setInterval(function() {
-      if (window.location.href.match(/\/groups\/\d+\/messages/)) {
-          var id = $('.chat-main__body__messageslist:last-child').data('messageId');
-          console.log(id);
-    $.ajax({
-    url: location.href,
-    dataType: 'json',
-    })
-    .done(function(json) {
-      var insertHTML = '';
-      json.forEach(function(message) {
-        if (message.id > id ) {
-          insertHTML += buildHTML(message);
-        }
-      });
-      $('.chat-main__body').append(insertHTML)
-       $('.html,body').animate({scrollTop: $('.chat-main__body')[0].scrollHeight}, 'fast');
-    })
-    .fail(function(json) {
-      alert('自動更新に失敗しました');
-    });
-  } else {
-    clearInterval(interval);
-   }} , 5 * 1000 );
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      var id = $('.chat-main__body__messageslist:last-child').data('messageId');
+        $.ajax({
+        url: location.href,
+        dataType: 'json',
+        })
+        .done(function(json) {
+        var insertHTML = '';
+        json.forEach(function(message) {
+          if (message.id > id ) {
+            insertHTML += buildHTML(message);
+          }
+          });
+          $('.chat-main__body').append(insertHTML)
+          $('.html,body').animate({scrollTop: $('.chat-main__body')[0].scrollHeight}, 'fast');
+        })
+        .fail(function(json) {
+          alert('自動更新に失敗しました');
+        });
+    } else {
+      clearInterval(interval);
+     }} , 5 * 1000 );
 });
